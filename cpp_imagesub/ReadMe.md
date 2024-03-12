fb642670f09db26230469173dcbb7e9ffa4b31db
Sri Hasitha <115046466+SriHasitha@users.noreply.github.com>
2024-03-11 20:31:47 -0400

    Added more packages

---
+++ cpp_imagesub/ReadMe.md
@@ -0,0 +1,10 @@
+# ReadMe file - cpp_imagesub
+*Made by ChatterArm team*
+
+# Package run
+
+```
+ros2 run cpp_imagesub image_subscriber
+```
+
+
---
+++ cpp_imagesub/src/image_subscriber.cpp
@@ -0,0 +1,41 @@
+#include <memory>
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/point_cloud2.hpp"
+#include "sensor_msgs/msg/image.hpp"
+
+using std::placeholders::_1;
+class ImageSubscriber : public rclcpp::Node
+{
+  public:
+    ImageSubscriber()
+    : Node("image_subscriber")
+    {
+      subscription_pointcloud_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
+      "/realsense/points", 10, std::bind(&ImageSubscriber::pointcloud_callback, this, _1));
+      
+      subscription_depthimage_ = this->create_subscription<sensor_msgs::msg::Image>(
+      "/realsense/depth/image_raw", 10, std::bind(&ImageSubscriber::depthimage_callback, this, _1));      
+    }
+
+  private:
+    void pointcloud_callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg) const
+    {
+      RCLCPP_INFO(this->get_logger(), "Received point cloud with %d points", msg->width * msg->height);
+    }
+    
+    void depthimage_callback(const sensor_msgs::msg::Image::SharedPtr msg) const
+    {
+      RCLCPP_INFO(this->get_logger(), "Received depth image with %d width and %d height", msg->width, msg->height);
+    }
+    
+    rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr subscription_pointcloud_;
+    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_depthimage_;	
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv); 
+  rclcpp::spin(std::make_shared<ImageSubscriber>());
+  rclcpp::shutdown();
+  return 0;
+}
