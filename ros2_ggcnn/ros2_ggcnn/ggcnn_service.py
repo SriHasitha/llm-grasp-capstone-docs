0cd4d3b5f8157bc76b1ca65c691a3c3e45e88990
Sri Hasitha <115046466+SriHasitha@users.noreply.github.com>
2024-04-20 23:22:18 -0400

    Update ggcnn_service.py
    pose in world frame

--- ros2_ggcnn/ros2_ggcnn/ggcnn_service.py
+++ ros2_ggcnn/ros2_ggcnn/ggcnn_service.py
@@ -1,13 +1,18 @@
 import rclpy
 from rclpy.node import Node
 
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image,CameraInfo
 from ggcnn_interface.srv import GraspPrediction
 from ros2_ggcnn.ggcnn_torch import process_depth_image,predict
 
 import numpy as np
 import cv2
 from cv_bridge import CvBridge
+
+from tf2_ros.buffer import Buffer
+from tf2_ros.transform_listener import TransformListener
+from geometry_msgs.msg import TransformStamped
+from scipy.spatial.transform import Rotation as R
 
 class GGCNNservice(Node):
     def __init__(self):
@@ -16,26 +21,55 @@ class GGCNNservice(Node):
         # init image subscribers
         self.rgb_sub = self.create_subscription(Image,'/color/image_raw',self.rgb_callback,1)
         self.depth_sub = self.create_subscription(Image,'/depth/image_raw',self.depth_callback,1)
+        # init cam_info subscriber to obtain intrinsic matrix
+        self.depth_info_sub = self.create_subscription(CameraInfo,'/depth/camera_info',self.depth_info_callback,1)
         self.get_logger().info('Initialized subscribers')
+        
         # init ggcnn service
         self.srv = self.create_service(GraspPrediction,'grasp_prediction',self.grasp_prediction_callback)
         self.get_logger().info('Initialized service')
 
+        # depth camera params
+        self.K_depth = []
+        self.depth_scale = 1000
+        self.h_depth = 0
+        self.w_depth = 0
+
         # to check if depth image was received
-        self.received = False
-        # self.depth = np.zeros((300,300))
+        self.received_depth = False
+        self.received_rgb = False
+        self.received_depth_K = False
+        # cv bridge to convert imgmsg to cv2img
         self.cv_bridge = CvBridge()
 
     def rgb_callback(self,img_msg):
         self.rbg = self.cv_bridge.imgmsg_to_cv2(img_msg)
-        if not self.received:
-            self.get_logger().info(f'Received RGB image: {img_msg.height}X{img_msg.width}')
+        if not self.received_rgb:
+            self.get_logger().info(f'Received RGB image: {img_msg.width}X{img_msg.height}')
+        self.received_rgb = True
 
     def depth_callback(self,img_msg):
         self.depth = self.cv_bridge.imgmsg_to_cv2(img_msg)
-        if not self.received:
-            self.get_logger().info(f'Received depth image: {img_msg.height}X{img_msg.width}')
-        self.received = True
+        self.h_depth = img_msg.height
+        self.w_depth = img_msg.width
+
+        if not self.received_depth:
+            self.get_logger().info(f'Received depth image: {self.w_depth}X{self.h_depth}')
+        self.received_depth = True
+    
+    def depth_info_callback(self,info_msg):
+        self.K_depth = info_msg.k
+        if not self.received_depth_K:
+            self.get_logger().info(f'Received Depth Camera Intrinsic Matrix:\n {self.K_depth}')
+        self.received_depth_K = True
+
+    # function to obtain transformation between frames       
+    def tf_transformation(self,from_frame,to_frame):
+        self.time = rclpy.time.Time()
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer, self)
+        self.transformation: TransformStamped = self.tf_buffer.lookup_transform(to_frame, from_frame, self.time)
+        self.received_transformation = True
 
     def show_image(self,img,title):
         img = cv2.normalize(img, None, 0, 255, cv2.NORM_MINMAX)
@@ -45,30 +79,83 @@ class GGCNNservice(Node):
     
     def grasp_prediction_callback(self,request,response):
         # Wait for the depth image to be received
-        while not self.received:
+        while not self.received_depth:
             self.get_logger().info('Waiting for depth image...')
             rclpy.spin_once(self, timeout_sec=0.1)  # Briefly yield control to allow other callbacks to run
 
+        # crop params
+        crop_size = 300
+        out_size = 300
+        crop_offset = 0
         # crop depth image
         depth = self.depth.copy()
-        depth_crop = process_depth_image(depth)
+        depth_crop = process_depth_image(depth,crop_size=crop_size,out_size=out_size,crop_y_offset=crop_offset)
         # show images
         self.show_image(depth,'Depth Image')
+        # self.get_logger().info('waiting to print cropped depth image')
+        # self.get_logger().info(f'cropped depth image: {self.depth_crop}')
+        # self.get_logger().info('waiting to print cropped depth image')
         self.show_image(depth_crop,'Cropped Depth Image')
 
-        self.get_logger().info('Predicting Grasp Pose')
         # predict grasp
-        points,angle,width_img,_ = predict(depth_crop)
+        self.get_logger().info('Predicting Grasp Pose')
+        points_out,angle_out,width_out = predict(depth_crop)
 
+        # Calculate  depth
+        # Figure out roughly the depth in mm of the part between the grippers for collision avoidance.
+        depth_center = depth_crop[100:141, 130:171].flatten()
+        depth_center.sort()
+        depth_center = depth_center[:10].mean() * self.depth_scale
+
+        # Get max quality pixel index from points_out
+        max_pixel = np.array(np.unravel_index(np.argmax(points_out), points_out.shape))
+
+        # get angle and width corresponding to above max_pixel
+        ang = angle_out[max_pixel[0], max_pixel[1]]
+        width = width_out[max_pixel[0], max_pixel[1]]
+
+        # Convert max_pixel back to uncropped/resized image coordinates in order to do the camera transform.
+        max_pixel = ((np.array(max_pixel) / out_size * crop_size) + np.array([(self.h_depth - crop_size)//2 - crop_offset, (self.w_depth - crop_size) // 2]))
+        max_pixel = np.round(max_pixel).astype(int)
+        self.get_logger().info(f'max_pixel: {max_pixel}')
+
+        point_depth = depth[max_pixel[0], max_pixel[1]]/self.depth_scale  # depth value in meters
+
+        # Compute the actual position.
+        fx = self.K_depth[0]
+        fy = self.K_depth[4]
+        cx = self.K_depth[2]
+        cy = self.K_depth[5]
+        x_cam = (max_pixel[1] - cx)/(fx) * point_depth  # meters
+        y_cam = (max_pixel[0] - cy)/(fy) * point_depth  # meters
+        z_cam = point_depth                             # meters
+
+        # Tranformation matrix from camera to world frame
+        camera_pose = self.tf_transformation('d435_depth_frame','world')
+        camera_p = np.array([camera_pose.transform.translation.x, camera_pose.transform.translation.y, camera_pose.transform.translation.z])
+        camera_rot = R.as_matrix(R.from_quat([
+                camera_pose.transform.rotation.x,
+                camera_pose.transform.rotation.y,
+                camera_pose.transform.rotation.z,
+                camera_pose.transform.rotation.w
+            ]))
+        
+        pos = np.dot(camera_rot, np.stack((x_cam, y_cam, depth_crop.flatten()))).T + np.array([[camera_p.x, camera_p.y, camera_p.z]])
+        self.get_logger().info(f'xyz in world frame: {pos}')
+       
         # convert to 3d pose
         best_grasp_pose = []
+
+        x = pos[0]
+        y = pos[1]
+        z = pos[2]
         
         # construct response
         response.success = True
-        # g = response.best_grasp
-        # g.pose.position.x = 0.0
-        # g.pose.position.y = 0.0
-        # g.pose.position.z = 0.0
+        g = response.best_grasp
+        g.pose.position.x = x
+        g.pose.position.y = y
+        g.pose.position.z = z.astype(float)
         # g.pose.orientation = 0.0
         # g.width = 0.0
         # g.quality = 0.0
